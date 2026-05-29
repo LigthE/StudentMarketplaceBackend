6bbd4d6de3f79c6f1b837763618eb1567ed476eb
Erkan Akdag <mafia001oh@gmail.com>
2026-05-29 09:42:59 +0200

    jag fixade validateProduct så den kollar put också och la till user/order i postman

--- src/middleware/validateProduct.ts
+++ src/middleware/validateProduct.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 export const validateProduct = (req: Request, res: Response, next: NextFunction) => {
   const method = req.method;
   
-  if (method === "POST") {
+  if (method === "POST" || method === "PUT") {
     const { name, description, price, rentPrice, category } = req.body;
     if (!name || !description || !price || !rentPrice || !category) {
       return res.status(400).json({ message: "Missing required fields" });
